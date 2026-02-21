3a9c32fe810d2e250d3f106a21ec1f2552f10e2e
Robson Vieira <robsonestudos883@gmail.com>
2026-02-20 23:32:07 -0300

    feat: Adicionando a rota do render

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,22 @@
 import axios from "axios";
 import Cookies from "js-cookie"; // Importe o js-cookie para manter compatibilidade com seu AuthContext
-
+/*
 const api = axios.create({
   baseURL: "http://localhost:8080", 
   headers: {
     "Content-Type": "application/json",
   },
 });
+*/
 
-// Interceptador para injetar o Token
+const api = axios.create({
+  baseURL: "https://stock-manager-19c3.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+
 api.interceptors.request.use((config) => {
   // Pega o token do Cookie (mesmo lugar onde o AuthContext salvou)
   const token = Cookies.get("token"); 
